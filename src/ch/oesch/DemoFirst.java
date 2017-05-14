2a5c465f0490883cf5a9194fbe9e59ecbb222da3
Walter Oesch <walter.oesch@outlook.com>
2017-05-14 23:52:13 +0200

    Push

--- src/ch/oesch/DemoFirst.java
+++ src/ch/oesch/DemoFirst.java
@@ -4,6 +4,8 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.EntityManager;
+
 
 /**
  * Created by Walter Oesch on 14.05.2017.
@@ -12,21 +14,38 @@ public class DemoFirst {
     public static void main(String[] args) {
 
         try {
-            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-            Session session = sessionFactory.openSession();
-            session.beginTransaction();
-
-            Student student = new Student();
-            student.setFirstName("Walter");
-            student.setAge(43);
-
-            session.save(student);
-            session.getTransaction().commit();
-
-            session.close();
-        } catch (HibernateException e) {
+            HibernateNative();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static void HibernateNative() throws Exception {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Student student = new Student();
+        student.setFirstName("Walter");
+        student.setAge(43);
+
+        session.save(student);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    private static void JpaHibernateImplementation() throws Exception {
+        EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Student student = new Student();
+        student.setFirstName("Eva");
+        student.setAge(43);
+        entityManager.persist(student);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 }
--- src/ch/oesch/HibernateUtil.java
+++ src/ch/oesch/HibernateUtil.java
@@ -4,16 +4,25 @@ import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+// JPA
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Created by Walter Oesch on 14.05.2017.
  */
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
+    private static final EntityManagerFactory entityManagerFactory;
     static {
         try {
             Configuration configuration=new Configuration();
             configuration.configure();
             sessionFactory = new Configuration().configure().buildSessionFactory();
+
+            // JPA
+            entityManagerFactory = Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
+
         } catch (Exception e) {
             System.err.println("Initial SessionFactory creation failed." + e);
             throw new ExceptionInInitializerError(e);
@@ -22,5 +31,10 @@ public class HibernateUtil {
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+
+    // JPA
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
     }
 }
